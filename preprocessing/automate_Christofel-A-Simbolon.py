e8b9684911f744def8fd630b1fad7b8c8c778560
Christofel2 <christofelssimbolon@gmail.com>
2025-11-18 18:30:08 +0700

    Modifikasi File Automate

--- preprocessing/automate_Christofel-A-Simbolon.py
+++ preprocessing/automate_Christofel-A-Simbolon.py
@@ -7,27 +7,23 @@ from joblib import dump, load
 import pandas as pd
 
 def preprocess_data(data, target_column, preprocessor_path, output_path):
-
-
+    #Drop Kolom name dan city serta duplikasi data
     drop_cols = ["name", "city"]
     data = data.drop(columns=drop_cols, errors="ignore")
-
-
     data = data.drop_duplicates()
 
-
+    # Pisah fitur dan target
     X = data.drop(columns=[target_column])
     y = data[target_column]
 
-
+    # Definisikan kolom numerik yang akan dipakai
     num_cols = ["income", "loan_amount", "points", "credit_score", "years_employed"]
 
-  
+    # Buat pipeline untuk preprocessing data numerik
     num_transformer = Pipeline(steps=[
         ("imputer", SimpleImputer(strategy="mean")),     
         ("scaler", RobustScaler())                        
     ])
-
     preprocessor = ColumnTransformer(
         transformers=[
             ("num", num_transformer, num_cols)
@@ -35,12 +31,13 @@ def preprocess_data(data, target_column, preprocessor_path, output_path):
         remainder="drop"
     )
 
-
+    # Set output ke pandas DataFrame jika memungkinkan
     try:
         preprocessor.set_output(transform="pandas")
     except:
         pass
 
+    # Split data menjadi train dan test set 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, stratify=y, test_size=0.2, random_state=42
     )
@@ -48,10 +45,9 @@ def preprocess_data(data, target_column, preprocessor_path, output_path):
     X_train_t = preprocessor.fit_transform(X_train)
     X_test_t = preprocessor.transform(X_test)
 
-
+    # Simpan preprocessor ke file
     dump(preprocessor, preprocessor_path)
-
-
+    # Simpan data train yang sudah dipreproses ke file CSV
     df_train = X_train_t.copy()
     df_train[target_column] = y_train.values
     df_train.to_csv(output_path, index=False)
@@ -66,7 +62,7 @@ def inference(data, preprocessor_path):
     pre = load(preprocessor_path)
     return pre.transform(data)
 
-
+## Menjalankan proses preprocessing otomatis untuk membaca dataset mentah, memprosesnya, lalu menyimpan pipeline dan data hasil preprocessing.
 if __name__ == "__main__":
     data = pd.read_csv("loan_approval_raw/loan_approval.csv")
     target_column = "loan_approved"
